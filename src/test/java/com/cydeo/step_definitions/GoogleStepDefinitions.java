8117f9292d764cdebb10f2d97314e1ae300018ef
Muhammed <arsenalkartal@gmail.com>
2022-10-04 00:03:37 +0300

    Practice CukesRunner , GoogleSearch , POM

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,19 +1,29 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
+   GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
+
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER );
 
     }
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
+        String expectedTitle = "apple - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertEquals(actualTitle , expectedTitle );
     }
 
     @When("user in on Google search page")
